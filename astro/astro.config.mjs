0cce372a62ce7d809e72228956502035abd16430
michsco <scottm@michaelbscott.com>
2025-05-25 14:47:37 -0700

    Fix build issue

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -5,56 +5,57 @@ import sitemap from '@astrojs/sitemap';
 import path from 'path';
 import starlightLinksValidator from 'starlight-links-validator'
 
-import vue from '@astrojs/vue';
-
 export default defineConfig({
-    site: 'https://biblequiz.com',
-    integrations: [sitemap(), starlight({
-        title: 'BibleQuiz.com',
-        description: 'Home of AG Bible Quiz',
-        social: [
-            { icon: 'email', label: 'Email', href: 'mailto:hello@biblequiz.com' },
-            { icon: 'facebook', label: 'Facebook', href: 'https://facebook.com/groups/agbiblequiz' },
-            { icon: 'youtube', label: 'Youtube', href: 'https://youtube.com/@BibleQuiz-AG' },
-            {
-              icon: 'email',
-              label: 'foo',
-              href: '/login',
-            }
-        ],
-        favicon: '/favicon.png',
-        logo: {
-            light: './src/assets/images/light-logo.png',
-            dark: './src/assets/images/dark-logo.png',
-            replacesTitle: true,
-        },
-        customCss: [
-          // Relative path to your custom CSS file
-          './src/styles/custom.css',
-        ],
-        components: {
-            Header: './src/components/Header.astro',
-            Footer: './src/components/Footer.astro',
-            LinkCardWithSlot: './src/components/LinkCardWithSlot.astro',
-        },
-        lastUpdated: true,
-        pagination: false,
-        plugins: [starlightLinksValidator()],
-		}), vue()],
-    output: 'static',
-    build: {
-        format: 'directory',
-        assets: '_astro',
-    },
-    vite: {
-      resolve: {
-        alias: {
-          '@': path.resolve('./src'),
-          '@assets': path.resolve('./src/assets'),
-          '@components': path.resolve('./src/components'),
-          '@layouts': path.resolve('./src/layouts'),
-          '@pages': path.resolve('./src/pages'),
-        }
-      }
-    }
+	site: 'https://biblequiz.com',
+	integrations: [
+		sitemap(),
+		starlight({
+			title: 'BibleQuiz.com',
+			description: 'Home of AG Bible Quiz',
+			social: [
+				{ icon: 'email', label: 'Email', href: 'mailto:hello@biblequiz.com' },
+				{ icon: 'facebook', label: 'Facebook', href: 'https://facebook.com/groups/agbiblequiz' },
+				{ icon: 'youtube', label: 'Youtube', href: 'https://youtube.com/@BibleQuiz-AG' },
+				{
+				  icon: 'email',
+				  label: 'foo',
+				  href: '/login',
+				}
+			],
+			favicon: '/favicon.png',
+			logo: {
+				light: './src/assets/images/light-logo.png',
+				dark: './src/assets/images/dark-logo.png',
+				replacesTitle: true,
+			},
+			customCss: [
+			  // Relative path to your custom CSS file
+			  './src/styles/custom.css',
+			],
+			components: {
+				Header: './src/components/Header.astro',
+				Footer: './src/components/Footer.astro',
+				LinkCardWithSlot: './src/components/LinkCardWithSlot.astro',
+			},
+			lastUpdated: true,
+			pagination: false,
+			plugins: [starlightLinksValidator()],
+		}),
+	],
+	output: 'static',
+	build: {
+		format: 'directory',
+		assets: '_astro',
+	},
+	vite: {
+	  resolve: {
+		alias: {
+		  '@': path.resolve('./src'),
+		  '@assets': path.resolve('./src/assets'),
+		  '@components': path.resolve('./src/components'),
+		  '@layouts': path.resolve('./src/layouts'),
+		  '@pages': path.resolve('./src/pages'),
+		}
+	  }
+	}
 });
