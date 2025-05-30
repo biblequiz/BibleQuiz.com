a510a9ebf06aa22b679c017af0bd14f4d6b1161e
michsco <scottm@michaelbscott.com>
2025-05-30 16:57:54 -0700

    Fix the blog

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -11,56 +11,59 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 
 export default defineConfig({
-    site: 'https://biblequiz.com',
-    integrations: [sitemap(), starlight({
-        title: 'BibleQuiz.com',
-        description: 'Home of AG Bible Quiz',
-        social: [
-            { icon: 'email', label: 'Email', href: 'mailto:hello@biblequiz.com' },
-            { icon: 'facebook', label: 'Facebook', href: 'https://facebook.com/groups/agbiblequiz' },
-            { icon: 'youtube', label: 'Youtube', href: 'https://youtube.com/@BibleQuiz-AG' },
-            {
-              icon: 'email',
-              label: 'foo',
-              href: '/login',
-            }
-        ],
-        favicon: '/favicon.png',
-        logo: {
-            light: './src/assets/images/light-logo.png',
-            dark: './src/assets/images/dark-logo.png',
-            replacesTitle: true,
-        },
-        customCss: [
-          './src/styles/custom.css',
-          './src/styles/global.css',
-        ],
-        components: {
-            Header: './src/components/Header.astro',
-            Footer: './src/components/Footer.astro',
-            LinkCardWithSlot: './src/components/LinkCardWithSlot.astro',
-        },
-        lastUpdated: true,
-        pagination: false,
-        plugins: [starlightLinksValidator(), starlightBlog()],
-    }), react()],
-    output: 'static',
-    build: {
-        format: 'directory',
-        assets: '_astro',
+  site: 'https://biblequiz.com',
+  integrations: [sitemap(), starlight({
+    title: 'BibleQuiz.com',
+    description: 'Home of AG Bible Quiz',
+    social: [
+      { icon: 'email', label: 'Email', href: 'mailto:hello@biblequiz.com' },
+      { icon: 'facebook', label: 'Facebook', href: 'https://facebook.com/groups/agbiblequiz' },
+      { icon: 'youtube', label: 'Youtube', href: 'https://youtube.com/@BibleQuiz-AG' },
+      {
+        icon: 'email',
+        label: 'foo',
+        href: '/login',
+      }
+    ],
+    favicon: '/favicon.png',
+    logo: {
+      light: './src/assets/images/light-logo.png',
+      dark: './src/assets/images/dark-logo.png',
+      replacesTitle: true,
     },
-    vite: {
-      resolve: {
-        alias: {
-          '@': path.resolve('./src'),
-          '@assets': path.resolve('./src/assets'),
-          '@components': path.resolve('./src/components'),
-          '@data': path.resolve('./src/data'),
-          '@layouts': path.resolve('./src/layouts'),
-          '@pages': path.resolve('./src/pages'),
-        }
-      },
+    customCss: [
+      './src/styles/custom.css',
+      './src/styles/global.css',
+    ],
+    components: {
+      Header: './src/components/Header.astro',
+      Footer: './src/components/Footer.astro',
+      LinkCardWithSlot: './src/components/LinkCardWithSlot.astro',
+    },
+    lastUpdated: true,
+    pagination: false,
+    plugins: [starlightLinksValidator(), starlightBlog({
+      postCount: 10,
+      recentPostCount: 10
+    })],
+  }), react()],
+  output: 'static',
+  build: {
+    format: 'directory',
+    assets: '_astro',
+  },
+  vite: {
+    resolve: {
+      alias: {
+        '@': path.resolve('./src'),
+        '@assets': path.resolve('./src/assets'),
+        '@components': path.resolve('./src/components'),
+        '@data': path.resolve('./src/data'),
+        '@layouts': path.resolve('./src/layouts'),
+        '@pages': path.resolve('./src/pages'),
+      }
+    },
 
-      plugins: [tailwindcss()]
-    }
+    plugins: [tailwindcss()]
+  }
 });
