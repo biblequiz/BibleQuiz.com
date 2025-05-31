8d6490d25705389828c2ca2d9d6ebb220a9208bc
michsco <scottm@michaelbscott.com>
2025-05-31 05:31:00 -0700

    Fix the sidebar

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -12,45 +12,72 @@ import react from '@astrojs/react';
 
 export default defineConfig({
   site: 'https://biblequiz.com',
-  integrations: [sitemap(), starlight({
-    title: 'BibleQuiz.com',
-    description: 'Home of AG Bible Quiz',
-    social: [
-      { icon: 'email', label: 'Email', href: 'mailto:hello@biblequiz.com' },
-      { icon: 'facebook', label: 'Facebook', href: 'https://facebook.com/groups/agbiblequiz' },
-      { icon: 'youtube', label: 'Youtube', href: 'https://youtube.com/@BibleQuiz-AG' },
-      {
-        icon: 'email',
-        label: 'foo',
-        href: '/login',
-      }
-    ],
-    favicon: '/favicon.png',
-    logo: {
-      light: './src/assets/images/light-logo.png',
-      dark: './src/assets/images/dark-logo.png',
-      replacesTitle: true,
-    },
-    customCss: [
-      './src/styles/custom.css',
-      './src/styles/global.css',
-    ],
-    components: {
-      Header: './src/components/Header.astro',
-      Footer: './src/components/Footer.astro',
-      LinkCardWithSlot: './src/components/LinkCardWithSlot.astro',
-    },
-    lastUpdated: true,
-    pagination: false,
-    plugins: [
-      starlightLinksValidator({
-        errorOnRelativeLinks: false
-      }),
-      starlightBlog({
-        postCount: 10,
-        recentPostCount: 10
-      })],
-  }), react()],
+  integrations: [
+    sitemap(),
+    starlight({
+      title: 'BibleQuiz.com',
+      description: 'Home of AG Bible Quiz',
+      social: [
+        { icon: 'email', label: 'Email', href: 'mailto:hello@biblequiz.com' },
+        { icon: 'facebook', label: 'Facebook', href: 'https://facebook.com/groups/agbiblequiz' },
+        { icon: 'youtube', label: 'Youtube', href: 'https://youtube.com/@BibleQuiz-AG' },
+        {
+          icon: 'email',
+          label: 'foo',
+          href: '/login',
+        }
+      ],
+      favicon: '/favicon.png',
+      logo: {
+        light: './src/assets/images/light-logo.png',
+        dark: './src/assets/images/dark-logo.png',
+        replacesTitle: true,
+      },
+      customCss: [
+        './src/styles/custom.css',
+        './src/styles/global.css',
+      ],
+      sidebar: [
+        {
+          label: 'Upcoming & Live Events',
+          slug: 'upcoming-events'
+        },
+        {
+          label: 'Apps',
+          slug: 'apps'
+        },
+        {
+          label: 'Subscribe',
+          slug: 'subscribe'
+        },
+        {
+          label: 'Junior Bible Quiz (JBQ)',
+          collapsed: true,
+          autogenerate: { directory: 'jbq'}
+        },
+        {
+          label: 'Teen Bible Quiz (TBQ)',
+          collapsed: true,
+          autogenerate: { directory: 'tbq'}
+        },
+      ],
+      components: {
+        Header: './src/components/Header.astro',
+        Footer: './src/components/Footer.astro',
+        LinkCardWithSlot: './src/components/LinkCardWithSlot.astro',
+      },
+      lastUpdated: true,
+      pagination: false,
+      plugins: [
+        starlightLinksValidator({
+          errorOnRelativeLinks: false
+        }),
+        starlightBlog({
+          postCount: 10,
+          recentPostCount: 10
+        })],
+    }),
+    react()],
   output: 'static',
   build: {
     format: 'directory',
