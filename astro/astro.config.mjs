a32fb57134d0f5509d319e1a37eec5b5ed8a6319
michsco <scottm@michaelbscott.com>
2025-06-01 16:18:13 -0700

    Add more pages.

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -64,7 +64,7 @@ export default defineConfig({
       lastUpdated: true,
       pagefind: {
         ranking: {
-          termSimilarity: 0.4
+          termSimilarity: 0.6
         }
       },
       pagination: false,
