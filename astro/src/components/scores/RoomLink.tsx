07d40164a9c28d874447a912f026baf87af41225
michsco <scottm@michaelbscott.com>
2025-07-05 08:00:47 -0600

    Fix RoomLink

--- astro/src/components/scores/RoomLink.tsx
+++ astro/src/components/scores/RoomLink.tsx
@@ -8,9 +8,15 @@ interface Props {
     children: React.ReactNode;
 };
 
+
 export default function RoomLink({ label, eventId, databaseId, meetId, matchId, roomId, children }: Props) {
+
+    const handleClick = () => {
+        alert(`Show the Room: ${label}\nEvent ID: ${eventId}\nDatabase ID: ${databaseId}\nMeet ID: ${meetId}\nMatch ID: ${matchId}\nRoom ID: ${roomId}`);
+    };
+
     return (
-        <a style={{ cursor: "pointer" }}>
+        <a style={{ cursor: "pointer" }} onClick={handleClick}>
             {children}
         </a>
     );
