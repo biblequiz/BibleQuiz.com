86a6977c33775857347c10f291d0fdd23de3e2bd
michsco <scottm@michaelbscott.com>
2025-07-11 10:41:33 -0700

    Add all the current events

--- astro/src/components/scores/EventScoringReportLoader.tsx
+++ astro/src/components/scores/EventScoringReportLoader.tsx
@@ -4,6 +4,7 @@ import type { EventInfo } from "@types/EventTypes";
 import { useStore } from "@nanostores/react";
 import { sharedEventScoringReportState, sharedPrintConfiguration } from "@utils/SharedState";
 import { PrintDialogModalId } from "./PrintDialogContent";
+import FontAwesomeIcon from "../FontAwesomeIcon";
 
 interface Props {
     parentTabId: string;
@@ -66,10 +67,8 @@ export default function EventScoringReportLoader({ parentTabId, tabSyncKey, even
                 parentTab.style.display = "none";
             }
 
-            return (<div role="alert" className="alert alert-error">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
-                </svg>
+            return (<div role="alert" className="alert alert-warning">
+                <FontAwesomeIcon icon="fas faTriangleExclamation" />
                 <span>{reportState.error}</span>
             </div>);
         }
