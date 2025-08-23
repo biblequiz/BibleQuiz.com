a2167066e667a6f1407a6789ba8ea2faf065c20c
michsco <scottm@michaelbscott.com>
2025-08-23 12:46:54 -0700

    Update the range

--- src/components/apps/questionGenerator/QuestionSelectorByRange.tsx
+++ src/components/apps/questionGenerator/QuestionSelectorByRange.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import type { QuestionRangeFilter } from "../../../types/services/QuestionGeneratorService";
 import settings from "../../../data/generated/questionGenerator.json";
 import FontAwesomeIcon from "../../FontAwesomeIcon";
@@ -16,20 +16,19 @@ interface AddRangeState {
 }
 
 const GENERATOR_SETTINGS = settings as JbqQuestionGeneratorSettings;
-const MIN_ELEMENT_ID = "range-new-min";
-const MAX_ELEMENT_ID = "range-new-max";
 
 export default function QuestionSelectorByRange({
     ranges,
     setRanges }: Props) {
 
     const [addState, setAddState] = useState<AddRangeState | null>(null);
+    const minInputRef = useRef<HTMLInputElement>(null);
+    const maxInputRef = useRef<HTMLInputElement>(null);
 
     useEffect(() => {
         if (addState && !addState.isFocused) {
-            const minElement = document.getElementById(MIN_ELEMENT_ID);
-            if (minElement) {
-                minElement.focus();
+            if (minInputRef.current) {
+                minInputRef.current.focus();
             }
 
             setAddState({ ...addState, isFocused: true });
@@ -44,34 +43,31 @@ export default function QuestionSelectorByRange({
     const completeAddRange = (event: React.MouseEvent<HTMLButtonElement>) => {
         event.preventDefault();
 
-        const minElement = document.getElementById(MIN_ELEMENT_ID) as HTMLInputElement;
-        const maxElement = document.getElementById(MAX_ELEMENT_ID) as HTMLInputElement;
-
         // Check the range before the other validity checks to ensure the check doesn't detect
         // an old failure.
         const firstQuestion = addState!.min as number;
         const lastQuestion = addState!.max as number;
         if (firstQuestion && lastQuestion) {
             if (firstQuestion > lastQuestion) {
-                minElement.setCustomValidity("Cannot be greater than the last question.");
+                minInputRef.current!.setCustomValidity("Cannot be greater than the last question.");
             }
             else {
-                minElement.setCustomValidity("");
+                minInputRef.current!.setCustomValidity("");
             }
 
             for (const existingRange of ranges) {
                 if (!(lastQuestion < existingRange.Start || firstQuestion > existingRange.End)) {
-                    minElement.setCustomValidity(`Overlaps with the range ${existingRange.Start}-${existingRange.End}.`);
+                    minInputRef.current!.setCustomValidity(`Overlaps with the range ${existingRange.Start}-${existingRange.End}.`);
                     break;
                 }
             }
         }
 
-        const isMinValid = minElement.checkValidity();
-        minElement.reportValidity();
+        const isMinValid = minInputRef.current!.checkValidity();
+        minInputRef.current!.reportValidity();
 
-        const isMaxValid = maxElement.checkValidity();
-        maxElement.reportValidity();
+        const isMaxValid = maxInputRef.current!.checkValidity();
+        maxInputRef.current!.reportValidity();
 
         if (!isMinValid || !isMaxValid) {
             return;
@@ -114,8 +110,7 @@ export default function QuestionSelectorByRange({
                 <div className="bg-base-200 border-base-400 rounded-box border p-4 mt-0">
                     <input
                         type="number"
-                        name={MIN_ELEMENT_ID}
-                        id={MIN_ELEMENT_ID}
+                        ref={minInputRef}
                         value={addState?.min ?? ""}
                         onChange={e => {
                             e.target.setCustomValidity("");
@@ -130,8 +125,7 @@ export default function QuestionSelectorByRange({
                     <FontAwesomeIcon icon="fas faMinus" classNames={["ml-2", "mr-2"]} />
                     <input
                         type="number"
-                        name={MAX_ELEMENT_ID}
-                        id={MAX_ELEMENT_ID}
+                        ref={maxInputRef}
                         value={addState?.max ?? ""}
                         onChange={e => {
                             e.target.setCustomValidity("");
