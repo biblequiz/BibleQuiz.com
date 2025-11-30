2d6d4ec93d45da8921a3a48b13e5263090844f1d
michsco <scottm@michaelbscott.com>
2025-11-30 09:20:16 -0800

    WIP

--- src/components/EventListFilters.tsx
+++ src/components/EventListFilters.tsx
@@ -1,13 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { useStore } from "@nanostores/react";
 import { $eventFilters, type EventFilterConfiguration } from "utils/SharedState";
-import EventScopeBadge from './EventScopeBadge.tsx';
 import FontAwesomeIcon from './FontAwesomeIcon';
 
 import type { RegionInfo, DistrictInfo } from 'types/RegionAndDistricts';
 import FilterDropdownButton from './FilterDropdownButton.tsx';
 import { DataTypeHelpers } from 'utils/DataTypeHelpers.ts';
-import { set } from 'date-fns';
 
 interface Props {
   regions: RegionInfo[];
@@ -86,7 +84,7 @@ export default function EventListFilters({ regions, districts }: Props) {
         newDistrictId = parts[1];
       }
     }
-    
+
     const newScope = newRegionId || newDistrictId
       ? { scope: selectedValue, label: e.target.options[e.target.selectedIndex].text }
       : undefined;
@@ -123,95 +121,90 @@ export default function EventListFilters({ regions, districts }: Props) {
   const { searchText, typeFilter } = currentEventFilters;
 
   return (
-    <fieldset className="fieldset bg-base-100 border-base-300 rounded-box border p-4 pb-2 pt-0">
-      <legend className="fieldset-legend">Event Search Criteria</legend>
-      <div className="mt-2">
-        <label className="input w-lg mt-0">
-          <FontAwesomeIcon icon="fas faSearch" classNames={["h-[1em]", "opacity-50"]} />
-          <input
-            type="text"
-            className="grow"
-            placeholder="Name or Location"
-            value={searchText}
-            onChange={e => {
-              const currentValue = e.target.value;
-              const newText = DataTypeHelpers.isNullOrEmpty(currentValue)
-                ? undefined
-                : currentValue;
-              $eventFilters.setKey("searchText", newText);
-            }} />
-          {(searchText?.length ?? 0) > 0 && (
-            <button
-              className="btn btn-ghost btn-xs"
-              onClick={() => $eventFilters.setKey("searchText", undefined)}>
-              <FontAwesomeIcon icon="fas faCircleXmark" />
-            </button>)}
-        </label>
-      </div>
-      <div className="flex flex-wrap gap-2 mt-0 mb-0">
-        <FilterDropdownButton
-          id="event-list-filters-who"
-          label={`Who: Teams in ${scope?.label ?? "All Districts"}`}
-          buttons={false}
-        >
-          <div>
-            <span>Display Teams in any of these:</span>
-            <div className="form-control">
-              <select
-                className="select select-sm"
-                onChange={handleScopeChanged}
-                value={scope?.scope}>
-                <option value="">
-                  All Districts
-                </option>
-                <option value="" disabled>
-                  ----------------------
-                </option>
-                {regions.map((region) => (
-                  <option key={`reg_${region.id}`} value={region.id}>
-                    {region.name} Region
-                  </option>
-                ))}
-                <option value="" disabled>
-                  ----------------------
-                </option>
-                {districts.map((district) => (
-                  <option key={`dis_${district.id}`} value={`${district.regionId}_${district.id}`}>
-                    {district.name} District
-                  </option>
-                ))}
-              </select>
-            </div>
-          </div>
-        </FilterDropdownButton>
-        <FilterDropdownButton
-          id="event-list-filters-what"
-          label={`What: ${typeFilter ? `${typeFilter.toUpperCase()} Events Only` : "JBQ or TBQ Events"}`}
-          buttons={false}
-        >
-          <div>
-            <div className="form-control">
-              <select
-                className="select select-sm"
-                onChange={handleTypeChanged}
-                value={typeFilter}>
-                <option value="">
-                  JBQ and TBQ Events
-                </option>
-                <option value="jbq">JBQ Events Only</option>
-                <option value="tbq">TBQ Events Only</option>
-              </select>
-            </div>
-          </div>
-        </FilterDropdownButton>
-        {(searchText || scope || typeFilter) && (
+    <div className="flex flex-wrap gap-2 mt-2 mb-0 align-middle">
+      <label className="input input-sm mt-2">
+        <FontAwesomeIcon icon="fas faSearch" classNames={["h-[1em]", "opacity-50"]} />
+        <input
+          type="text"
+          className="grow"
+          placeholder="Name or Location"
+          value={searchText}
+          onChange={e => {
+            const currentValue = e.target.value;
+            const newText = DataTypeHelpers.isNullOrEmpty(currentValue)
+              ? undefined
+              : currentValue;
+            $eventFilters.setKey("searchText", newText);
+          }} />
+        {(searchText?.length ?? 0) > 0 && (
           <button
-            type="button"
-            className="btn btn-sm btn-outline mt-2"
-            onClick={handleClearFilters}>
-            <FontAwesomeIcon icon={`fas faFilterCircleXmark`} />
-            Clear Filters
+            className="btn btn-ghost btn-xs"
+            onClick={() => $eventFilters.setKey("searchText", undefined)}>
+            <FontAwesomeIcon icon="fas faCircleXmark" />
           </button>)}
-      </div>
-    </fieldset>);
+      </label>
+      <FilterDropdownButton
+        id="event-list-filters-who"
+        label={`Who: Teams in ${scope?.label ?? "All Districts"}`}
+        buttons={false}
+      >
+        <div>
+          <span>Display Teams in any of these:</span>
+          <div className="form-control">
+            <select
+              className="select select-sm"
+              onChange={handleScopeChanged}
+              value={scope?.scope}>
+              <option value="">
+                All Districts
+              </option>
+              <option value="" disabled>
+                ----------------------
+              </option>
+              {regions.map((region) => (
+                <option key={`reg_${region.id}`} value={region.id}>
+                  {region.name} Region
+                </option>
+              ))}
+              <option value="" disabled>
+                ----------------------
+              </option>
+              {districts.map((district) => (
+                <option key={`dis_${district.id}`} value={`${district.regionId}_${district.id}`}>
+                  {district.name} District
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
+      </FilterDropdownButton>
+      <FilterDropdownButton
+        id="event-list-filters-what"
+        label={`What: ${typeFilter ? `${typeFilter.toUpperCase()} Events Only` : "JBQ or TBQ Events"}`}
+        buttons={false}
+      >
+        <div>
+          <div className="form-control">
+            <select
+              className="select select-sm"
+              onChange={handleTypeChanged}
+              value={typeFilter}>
+              <option value="">
+                JBQ and TBQ Events
+              </option>
+              <option value="jbq">JBQ Events Only</option>
+              <option value="tbq">TBQ Events Only</option>
+            </select>
+          </div>
+        </div>
+      </FilterDropdownButton>
+      {(searchText || scope || typeFilter) && (
+        <button
+          type="button"
+          className="btn btn-sm btn-outline mt-2"
+          onClick={handleClearFilters}>
+          <FontAwesomeIcon icon={`fas faFilterCircleXmark`} />
+          Clear Filters
+        </button>)}
+    </div>);
 }
