01a14389538562413bfa87504946df5b908b2c78
michsco <scottm@michaelbscott.com>
2025-12-06 09:03:46 -0800

    Add the changes requested by Kevin

--- src/components/scores/ScheduleGridTabContent.tsx
+++ src/components/scores/ScheduleGridTabContent.tsx
@@ -126,8 +126,8 @@ export default function ScheduleGridTabContent({
                                     {team.Scores!.Rank}{team.Scores!.IsTie ? '*' : ''}
                                 </td>)}
                             <td className="pl-0">
-                                <ToggleTeamOrQuizzerFavoriteButton type="team" id={team.Id} /><br />
-                                <span>{team.Name}</span>
+                                <ToggleTeamOrQuizzerFavoriteButton type="team" id={team.Id} />&nbsp;<span className="font-bold">{team.ChurchName}</span><br />
+                                <span className="italic">{team.Name}</span>
                             </td>
                             {hasRankedTeams && (
                                 <>
