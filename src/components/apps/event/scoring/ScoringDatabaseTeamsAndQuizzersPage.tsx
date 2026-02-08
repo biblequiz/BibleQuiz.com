43d43e0402b7beb5596f159c76c240861742684f
michsco <scottm@michaelbscott.com>
2026-02-07 17:14:25 -0800

    WIP

--- src/components/apps/event/scoring/ScoringDatabaseTeamsAndQuizzersPage.tsx
+++ src/components/apps/event/scoring/ScoringDatabaseTeamsAndQuizzersPage.tsx
@@ -126,19 +126,6 @@ export default function ScoringDatabaseTeamsAndQuizzersPage({ }: Props) {
                 />
             )}
             <div className="divider mt-0" />
-            {JSON.stringify(currentTeamsAndQuizzers)}
-            <div>
-                <b>Teams & Quizzers Section</b>
-            </div>
-            <p>
-                This page includes the following:
-            </p>
-            <ul>
-                <li>Manage Teams & Quizzers (like ScoreKeep).</li>
-                <li>Import Teams & Quizzers from the Registration (including updates). The user should be able to specify how teams should be named.</li>
-            </ul>
-            <div>
-                Placeholder for {auth.userProfile?.displayName}
-            </div>
+            TODO -- add control for the teams.
         </div>);
 }
