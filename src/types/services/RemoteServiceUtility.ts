adff970fbc30eed922fe09f02979bfc64ab52ee9
michsco <scottm@michaelbscott.com>
2026-09-14 09:49:34 -0700

    WIP

--- src/types/services/RemoteServiceUtility.ts
+++ src/types/services/RemoteServiceUtility.ts
@@ -309,7 +309,7 @@ export class RemoteServiceUtility {
         let baseUrl: string;
         switch (service) {
             case RemoteServiceUrlBase.Registration:
-                baseUrl = "http://localhost:5002"; // "https://registration.biblequiz.com";
+                baseUrl = "https://registration.biblequiz.com";
                 break;
             case RemoteServiceUrlBase.Scores:
                 baseUrl = "https://scores.biblequiz.com";
